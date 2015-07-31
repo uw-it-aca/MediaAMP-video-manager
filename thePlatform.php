bf557fa99fd27a65b09c4056f9cd3d40393ec71c
Dajia Bao <baod@uw.edu>
2015-07-31 14:29:41 -0700

    Changes 'mpx Video Manager' to 'Browse MPX Media'

--- thePlatform.php
+++ thePlatform.php
@@ -100,7 +100,7 @@ class ThePlatform_Plugin {
 	 * @return array The updated array of tabs
 	 */
 	function tp_upload_tab( $tabs ) {
-		$tabs['theplatform'] = "mpx Video Manager";
+		$tabs['theplatform'] = "Browse MPX Media";
 
 		return $tabs;
 	}
